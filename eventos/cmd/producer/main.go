add75126d55d579228f8c8b725be30d28ba5a299
Marcelo <marcelo.silva@neomind.com.br>
2024-06-24 20:00:38 -0300

    feat(events): Trabalhando com rabbitmq
    Criado producer e consumer + pkg do rabbitmq

---
+++ eventos/cmd/producer/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/masilvasql/eventos/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+
+	err = rabbitmq.Publish(ch, "Hello World", "amq.direct")
+	if err != nil {
+		panic(err)
+	}
+
+}
